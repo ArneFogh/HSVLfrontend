875016d007713de3ac27162082a1c669e0937bfc
nicklassienczak <nicklas-sk@hotmail.com>
2022-11-21 11:15:50 +0100

    activitiesData

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/activities')
+fetch('http://localhost:3000/activitiesData')
     .then(response => response.json())
     .then(HSVL => {
         activities(HSVL);
@@ -9,12 +9,14 @@ function activities(HSVL){
     const ul = document.querySelector("ul.activityList");
     for (let i = 0; i < HSVL.length; i++) {
         const activity = HSVL[i].activity;
-        const popularity = HSVL[i].popularity
+        const popularity = HSVL[i].popularity;
+        const adress = HSVL[i].location
         console.log(activity)
         const li = document.createElement("li");
         li.innerHTML = `
             <h2>${activity}</h2>
-            <p>Popularity: ${popularity}</p>         
+            <p>Popularity: ${popularity}</p>  
+            <p>Location: ${adress}</p>       
                         `;
         ul.appendChild(li);
     }
