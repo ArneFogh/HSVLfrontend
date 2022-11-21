3d4fde7a26434ddb8fc4c8e593f250ed18ec91dd
arnefogh <andreasfogh7@gmail.com>
2022-11-21 16:19:39 +0100

    Added cities to the frontend

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const activePage = window.location.pathname;
+console.log(activePage)
+
+const navLinks = document.querySelectorAll("nav a").forEach(link => {
+    if (!link.href.includes(`${activePage}`)) {
+        return;
+    }
+    console.log(`${activePage}`);
+    link.classList.add("active");
+});
+
 fetch('http://localhost:3000/activitiesData')
     .then(response => response.json())
     .then(HSVL => {
@@ -10,13 +21,15 @@ function activities(HSVL){
     for (let i = 0; i < HSVL.length; i++) {
         const activity = HSVL[i].activity;
         const popularity = HSVL[i].popularity;
-        const adress = HSVL[i].location
-        console.log(activity)
+        const adress = HSVL[i].location;
+        const city = HSVL[i].city;
+        console.log(activity);
         const li = document.createElement("li");
         li.innerHTML = `
             <h2>${activity}</h2>
-            <p>Popularity: ${popularity}</p>  
-            <p>Location: ${adress}</p>       
+            <p><span>Popularity:</span> ${popularity}</p>  
+            <p><span>Location:</span> ${adress}</p> 
+            <p><span>By:</span> ${city}</p>      
                         `;
         ul.appendChild(li);
     }
