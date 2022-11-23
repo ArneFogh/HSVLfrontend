f4fa2166368a55af320fa18024e3c52370231610
arnefogh <andreasfogh7@gmail.com>
2022-11-23 11:23:07 +0100

    updated searchbar

--- script.js
+++ script.js
@@ -15,12 +15,14 @@ const navLinks = document.querySelectorAll("nav a").forEach(link => {
 fetch('https://hsvl.onrender.com/data')
     .then(response => response.json())
     .then(HSVL => {
-        activities(HSVL);
+        renderActivities(HSVL);
+        registrerInput(HSVL);
     });
 
 // Listen med activities, printet på html i en ul.
-function activities(HSVL){
+function renderActivities(HSVL){
     const ul = document.querySelector("ul.activityList");
+    ul.innerHTML = "";
     for (let i = 0; i < HSVL.length; i++) {
         const activity = HSVL[i].activity;
         const popularity = HSVL[i].popularity;
@@ -67,68 +69,23 @@ function populareActivities(mostPopulare){
 
 }
 
-// Endpoint til data om cities, hvor vi sender en request til API.
-fetch('https://hsvl.onrender.com/cities')
-    .then(response => response.json())
-    .then(cities => {
-        registrerInput(cities);
-    });
 
-function registrerInput(cities){
-    const btn = document.querySelector('button.filter');
-    btn.addEventListener('click', function (){
-        console.log("button clicked");
-        const inputElement = document.querySelector('input#userInput');
-        const zipCode = inputElement.value;
 
-        const filteredLocations = [];
-        for (let i = 0; i < cities.length; i++) {
-            const citiesList = cities[i];
-            if (citiesList.zip_code < zipCode) {
-                filteredLocations.push(citiesList)
+
+function registrerInput(activities){
+    /*const btn = document.querySelector('button.filter'); */
+    const inputElement = document.querySelector('input#userInput')
+    inputElement.addEventListener('input', function (){
+        const activityName = inputElement.value.toLowerCase()
+        const filteredActivities = [];
+        for (let i = 0; i < activities.length; i++) {
+            console.log(activities[i])
+            if (activities[i].activity.toLowerCase().includes(activityName)){
+                console.log(activities[i].activity)
+                filteredActivities.push(activities[i])
             }
         }
-
-        console.log(filteredLocations)
+        renderActivities(filteredActivities)
     })
 }
 
-// const searchInput = document.querySelector("")
-
-/*
-const searchInput = document.getElementById("searchInput")
-const inputField = document.getElementById("inputField").value
-function searchFunc(jsonIMDB) {
-    searchInput.addEventListener("click", e => {
-
-        for (let i = 0; i < jsonIMDB.length; i++) {
-            let inputFromUser = inputField
-            let movieValue = jsonIMDB[i].title
-            if (inputFromUser.includes(movieValue)) {
-                let createResult = document.createElement("li")
-                createResult.innerHTML = movieValue
-                btnMovies.appendChild(createResult)
-            }
-        }
-    })
-}
-
-
-// lav inputs i html og button, alle med id. placeholder i inputs hvor der står, det der skal stå i feltet
-// få fat i dine input fields med doc.querySelector og giv dem en variabel
-//LOG DEM UD
-// få fat i knappen på samme måde
-const buttonName = document.querySelector()
-buttonName.addEventListener('click', createNewActivity());
-
-function createNewActivity() {
-    const text = textInput.value
-    const text2 = text2input.value
-    const text3 = text3input.value
-
-
-}
-
-// lav en const med keys af det der skal ind. body: JSON.stringify(*navn på const med keys*)
-
- */
